49d2f8308cf3b487f2d942ce0b28834347b7c1c2
Rob Dodson <lets.email.rob@gmail.com>
2013-10-09 12:06:05 -0700

    fixed highlights, add image to header and add in g+ link

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -30,12 +30,13 @@ module HighlightCode
     highlighted_code
   end
   def tableize_code (str, lang = '')
-    table = '<div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'
-    code = ''
-    str.lines.each_with_index do |line,index|
-      table += "<span class='line-number'>#{index+1}</span>\n"
-      code  += "<span class='line'>#{line}</span>"
-    end
-    table += "</pre></td><td class='code'><pre><code class='#{lang}'>#{code}</code></pre></td></tr></table></div>"
+    # table = '<div class="highlight"><table><tr><td class="gutter"><pre class="line-numbers">'
+    # code = ''
+    # str.lines.each_with_index do |line,index|
+    #   table += "<span class='line-number'>#{index+1}</span>\n"
+    #   code  += "<span class='line'>#{line}</span>"
+    # end
+    # table += "</pre></td><td class='code'><pre><code class='#{lang}'>#{code}</code></pre></td></tr></table></div>"
+    table = "<pre><code>#{str}</code></pre>"
   end
 end
